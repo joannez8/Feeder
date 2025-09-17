6074df646f3a47d7777237e808e671727d6901ce
joannez8 <joannez8@nycstudents.net>
2025-09-17 15:35:00 -0400

    Test cases for simulateOneDay

--- Main.java
+++ Main.java
@@ -2,9 +2,20 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder(250);
-        System.out.println(f.simulateManyDays(10,5) + " day(s)");
-        f = new Feeder(0);
-        System.out.println(f.simulateManyDays(5,10) + " day(s)");
+        //simulateOneDay test cases
+        Feeder f = new Feeder(500); //ex#1
+        f.simulateOneDay(12);
+        System.out.println(f.getCurrentFood());
+        f = new Feeder(1000); //ex#2
+        f.simulateOneDay(22);
+        System.out.println(f.getCurrentFood());
+        f = new Feeder(100); //ex#3
+        f.simulateOneDay(5);
+        System.out.println(f.getCurrentFood());
+
+        //simulateManyDays test cases
+        f = new Feeder(2400); //ex#1
+        f.simulateManyDays(10,4);
+
     }
 }
